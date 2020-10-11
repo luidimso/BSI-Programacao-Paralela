444b27ee58e30902b86752cb329ce90ef5ff0aca
luidimso <luidi_mso@outlook.com>
2020-10-11 09:32:17 -0300

    First commit

---
+++  BSI-PP-T1-Luidi-Tiago.py
@@ -0,0 +1,15 @@
+from threading import Thread
+
+TEXTOS = ["Programacao Paralela", "Instituto Federal Fluminense"]
+
+class Trabalho1(Thread):
+    def __init__ (self, index):
+        Thread.__init__(self)
+        self.index = index
+
+    def run(self):
+        print(TEXTOS[self.index])
+
+for texto_index in range (2):
+    thread = Trabalho1(texto_index)
+    thread.start()
