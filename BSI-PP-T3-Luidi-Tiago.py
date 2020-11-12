9672a6efd56e318c52bbc256a7a6bc3e65497175
Tiago Corrêa <tiago.correa@ucam-campos.br>
2020-11-12 14:57:44 -0300

    Little bug fix.

--- BSI-PP-T3-Luidi-Tiago.py
+++ BSI-PP-T3-Luidi-Tiago.py
@@ -1,50 +1,88 @@
 # -*- coding: utf-8 -*-
-#Luidi Matheus
 #Tiago Corrêa
+#Luidi Matheus
 
-import time
-from threading import Thread
-from random import randint
-import threading
-Semaphore = threading.Semaphore()
+import threading 
+import time 
+import random
+import sys
+sem = threading.Semaphore() 
+
+lock = 0
 
 numeroA = 0
 numeroB = 0
 loopQuantity = 0
-timeSleeping = 0
 
+#soma
+def fun1(a,b): 
+    global lock, loopQuantity
+    t = 0
+    while loopQuantity > 0: 
+        if lock == 0:
+            sem.acquire() 
+            print("SOMA = {r}".format(r = a+b))
 
-class Trabalho3(Thread):
-    def __init__ (self, operation, timeSleeping, semaphore, loopQuantity):
-        Thread.__init__(self)
-        self.operation = operation
-        self.time = timeSleeping
-        self.sem = semaphore
-        self.loopQuantity = loopQuantity
+            if t == 0:
+                t = random.randint(1,10)
+                print("Gerando tempo de descanco da thread de adicao....{tempo} segundos.".format(tempo = t))
 
-    def run(self):
-        print("Thread da operacao {operation} iniciada.".format(operation = self.operation))
-        for x in range(self.loopQuantity):
-            if self.operation == "+":
-                self.sem.acquire()
-                time.sleep(self.time)
-                print("Opa, thread de soma acabou após {time} segundos. O resultado é {result}".format(time=self.time, result=numeroA + numeroB))
-                self.sem.release()
-            elif self.operation == "-":
-                    self.sem.acquire()
-                    time.sleep(self.time)
-                    print("Opa, thread de subtração acabou após {time} segundos. O resultado é {result}".format(time=self.time, result=numeroA - numeroB))
-                    self.sem.release()
-            elif self.operation == "*":
-                    self.sem.acquire()
-                    time.sleep(self.time)
-                    print("Opa, thread de multiplicação acabou após {time} segundos. O resultado é {result}".format(time=self.time, result=numeroA * numeroB))
-                    self.sem.release()
-            else:
-                self.sem.acquire()
-                time.sleep(self.time)
-                print("Opa, thread de divisão acabou após {time} segundos. O resultado é {result}".format(time=self.time, result=numeroA / numeroB))
-                self.sem.release()
+            time.sleep(t) 
+            lock+=1
+            sem.release() 
+        
+#subtracao
+def fun2(a,b): 
+    global lock, loopQuantity
+    t = 0
+    while loopQuantity > 0: 
+        if lock == 1:
+            sem.acquire()
+            print("SUBTRACAO = {r}".format(r = a-b))
+            if t == 0:
+                t = random.randint(1,10)
+                print("Gerando tempo de descanço da thread de subtracao....{tempo} segundos.".format(tempo = t))
+
+            time.sleep(t)
+            lock+=1
+            sem.release()
+#multiplicacao
+def fun3(a,b): 
+    global lock, loopQuantity
+    t = 0
+    while loopQuantity > 0: 
+        if lock == 2:
+            sem.acquire()
+            print("MULTIPLICACAO = {r}".format(r = a*b))
+
+            if t == 0:
+                t = random.randint(1,10)
+                print("Gerando tempo de descanço da thread de multiplicacao....{tempo} segundos.".format(tempo = t))
+
+            time.sleep(t)
+            lock+=1
+            sem.release()
+#divisao
+def fun4(a,b): 
+    global lock, loopQuantity
+    t = 0
+    while loopQuantity > 0: 
+
+        if lock == 3:
+            loopQuantity -=1
+            sem.acquire()
+            print("DIVISAO = {r}".format(r = a/b))
+
+            if t == 0:
+                t = random.randint(1,10)
+                print("Gerando tempo de descanço da thread de divisão....{tempo} segundos.".format(tempo = t))
+
+            time.sleep(t)
+            lock = 0
+            sem.release()
+            if loopQuantity == 0:
+                sys.exit(0)
+
 
 while numeroA == 0:
     print("Informe o primeiro número?")
@@ -55,27 +93,15 @@ while numeroB == 0:
     numeroB = int(input())
 
 while loopQuantity == 0:
-    print("Qual é a quantidade de loops?")
+    print("Por quantas vezes o algoritimo vai se repetir?")
     loopQuantity = int(input())
 
-timeSleeping = randint(1, 10)
-print("Thread de soma vai ter um ciclo de {time} segundos".format(time=timeSleeping))
-threadSoma = Trabalho3("+", timeSleeping, Semaphore, loopQuantity)
-
-timeSleeping = randint(1, 10)
-print("Thread de subtração vai ter um ciclo de {time} segundos".format(time=timeSleeping))
-threadSubtracao = Trabalho3("-", timeSleeping, Semaphore, loopQuantity)
-
-timeSleeping = randint(1, 10)
-print("Thread de multiplicação vai ter um ciclo de {time} segundos".format(time=timeSleeping))
-threadMultiplicacao = Trabalho3("*", timeSleeping, Semaphore, loopQuantity)
-
-timeSleeping = randint(1, 10)
-print("Thread de divisão vai ter um ciclo de {time} segundos".format(time=timeSleeping))
-threadDivisao = Trabalho3("/", timeSleeping, Semaphore, loopQuantity)
-
-
-threadSoma.start()
-threadSubtracao.start()
-threadMultiplicacao.start()
-threadDivisao.start()
+print("----------------------------Iniciando threads....")
+t2 = threading.Thread(target = fun2, args=(numeroA, numeroB))
+t2.start()
+t1 = threading.Thread(target = fun1, args=(numeroA, numeroB))
+t1.start()
+t4 = threading.Thread(target = fun4, args=(numeroA, numeroB))
+t4.start()
+t3 = threading.Thread(target = fun3, args=(numeroA, numeroB))
+t3.start()
