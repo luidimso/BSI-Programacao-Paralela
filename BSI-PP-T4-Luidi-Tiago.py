57d25eff53a604239cadb4ed9e70caa366b6a72a
luidimso <luidi_mso@outlook.com>
2020-12-09 15:34:16 -0300

    Implementação das threads no T4

--- BSI-PP-T4-Luidi-Tiago.py
+++ BSI-PP-T4-Luidi-Tiago.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 #Tiago Corrêa
 #Luidi Matheus
+
+from threading import Semaphore, Thread
+sem = Semaphore(2)
       
 matriz = [[2, 0, 0],  
           [1, 4, 0],  
@@ -11,20 +14,33 @@ vetorResultado = [2,
                   -3,
                   1]
 
-def somatorio(index):
-    count = 0
-    if index != 0:
-        for i in range(index, 0, -1):
-            print(i, index)
-            count += matriz[index][i-1]*vetor[i-1]
+somatorio_resultado = 0
+index = 0
+
+def somatorio():
+    global index
+    while index < 3:
+        print("Somando")
+        count = 0
+        if index != 0:
+            for i in range(index, 0, -1):
+                count += matriz[index][i-1]*vetor[i-1]
+        somatorio_resultado = count
+    
+def calc():
+    global index
+    while index < 3:
+        print("Calculando")
+        resultado = (vetorResultado[index]-somatorio_resultado)/matriz[index][index]
+        vetor[index] = resultado
+        print(vetor)
+        index += 1
         
-    return count
     
-def calc(index):
-    resultado = (vetorResultado[index]-somatorio(index))/matriz[index][index]
-    vetor[index] = resultado
-    print(vetor)
-    
-calc(0)
-calc(1)
-calc(2)
+t1 = Thread(target = somatorio)
+t2 = Thread(target = calc)
+
+t1.start()
+t1.join()
+t2.start()
+t2.join()
